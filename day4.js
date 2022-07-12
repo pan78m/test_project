7c92ba1a16f6e3dbb6a0ebc7fbeca0765b3fd2a9
pan78m <aryanpankaj78@gmail.com>
2022-07-12 20:18:26 +0600

    t3 to t7

---
+++ day4.js
@@ -0,0 +1,39 @@
+/*
+var n1,n2;
+n1=20;n2=40;
+document.write("Num1=" +n1+" And Num2="+n2+"<br/>");
+// Library Function for String!!!
+var text="Bangladesh";
+var len=text.length;
+document.write("Number of Character ="+len+"<br/>");
+
+
+// use prompt key or inbox message for pop of!!
+text=prompt("Enter Your Name:");
+var len=text.length;
+document.write("Number of Character ="+len+"<br/>");
+
+//text value search at any point ?
+var n="Hello World!!";
+n=n.charAt(3);
+document.write(n+"<br/>");
+
+// converted lower to uper case?
+n= "Good World!@"
+var uper=n.toUpperCase();
+document.write(uper+"<br/>");
+
+// uper converted in lower case?
+var lower=n.toLowerCase();
+document.write(lower+"<br/>");
+
+//add to character value use function?
+var t="Bangladesh";
+var t2=" is the Reverin Country!!";
+document.write(t.concat(t2)+"<br/>");
+
+// use spices function??
+var sp="GOOD wORLD!!";
+var n=sp.slice(3,6);
+document.write(n);
+*/
